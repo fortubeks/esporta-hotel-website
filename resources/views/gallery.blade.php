37ec4eda69df2713dcb0209f94528923f77b6006
Fortune Bekee <fortubeks@gmail.com>
2025-02-21 14:41:42 +0100

    .

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -7,7 +7,7 @@
     <div id="top"></div>
 
     <section id="subheader" class="relative jarallax text-light">
-        <img src="almaris/images/background/room.jpg" class="jarallax-img" alt="">
+        <img src="almaris/images/background/room.JPG" class="jarallax-img" alt="">
         <div class="container relative z-index-1000">
             <div class="row justify-content-center">
                 <div class="col-lg-12 text-center">
@@ -26,181 +26,181 @@
         <div class="container">
             <div class="row g-4">
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/room.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/room.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/room.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/room.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/room1.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/room1.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/room1.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/room1.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/interior1.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/interior1.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/interior1.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/interior1.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/food-2.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/food-2.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/food-2.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/food-2.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/food-3.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/food-3.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/food-3.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/food-3.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/food-4.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/food-4.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/food-4.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/food-4.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/food-1.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/food-1.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/food-1.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/food-1.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/reception.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/reception.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/reception.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/reception.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/outdoor.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/outdoor.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/outdoor.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/outdoor.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/outdoor1.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/outdoor1.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/outdoor1.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/outdoor1.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/outdoor2.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/outdoor2.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/outdoor2.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/outdoor2.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/outdoor3.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/outdoor3.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/outdoor3.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/outdoor3.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/food-5.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/food-5.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/food-5.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/food-5.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/food-6.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/food-6.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/food-6.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/food-6.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-4 text-center">
-                    <a href="almaris/images/gallery/food-7.jpg" class="image-popup d-block hover">
+                    <a href="almaris/images/gallery/food-7.JPG" class="image-popup d-block hover">
                         <div class="relative overflow-hidden rounded-10">
                             <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
                                 <h4 class="mb-0 hover-scale-in-3">View</h4>
                             </div>
                             <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                            <img src="almaris/images/gallery/food-7.jpg" class="img-fluid hover-scale-1-2" alt="">
+                            <img src="almaris/images/gallery/food-7.JPG" class="img-fluid hover-scale-1-2" alt="">
                         </div>
                     </a>
                 </div>
